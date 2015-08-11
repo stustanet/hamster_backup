4eaa64129af37b96748a616a1a5e6a24f54845c8
Markus Kaindl <markus.kaindl@stustanet.de>
2015-08-11 12:30:30 +0200

    initial commit - client part

---
+++ client/README.md
@@ -0,0 +1,6 @@
+Hamster Backup System - Client
+==============================
+
+* Diese Dateien werden auf dem Client nach /etc/hamster_backups kopiert.
+* In der rsync_excl können weitere Verzeichnisse vom Backup ausgenommen werden.
+* Auf dem Client sollte in /etc/hamster_backups ein SSH-Key id_rsa_hamster liegen, dessen Publickey auf Hamster eingetragen ist
---
+++ client/hamster_backup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ROTATION_INDICATOR=/etc/hamster_backups/do_rotate
+
+rm -rf ${ROTATION_INDICATOR}
+echo "# Achtung! Diese Datei wird regelmäßig vom Hamster-Backup überschrieben" > ${ROTATION_INDICATOR}
+
+/usr/bin/rsync -aAE --delete --numeric-ids --exclude-from=/etc/hamster_backups/rsync_excl.list -e "ssh -i /etc/hamster_backups/id_rsa_hamster" / root@hamster.stusta.mhn.de:.
+EC=${?}
+if [ ${EC} -gt 0 ]; then
+	echo "Rsync failed, something went wrong.. :("
+	exit 2
+fi
+
+rm -rf ${ROTATION_INDICATOR}
